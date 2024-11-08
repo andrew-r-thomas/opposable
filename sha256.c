622576a81d7e0984832bf291f7b024cdb5a82149
Andrew Thomas <andrew@andrewrthomas.com>
2024-11-08 15:37:02 -0800

    we got a hash baby

--- sha256.c
+++ sha256.c
@@ -1,4 +1,34 @@
+/*
+	 TODO:
+	 - handle message blocks larger than one 512 bit chunk
+	 - figure out libc stuff like stdint and memcpy etc
+	 - add function to tell how much mem will be needed for whole hash,
+	   then allocate everything you will need all at start (on stack)
+*/
 #include <stdint.h>
+
+const uint32_t K[64] = {
+	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+};
+const uint32_t H[8] = {
+	0x6a09e667,
+	0xbb67ae85,
+	0x3c6ef372,
+	0xa54ff53a,
+	0x510e527f,
+	0x9b05688c,
+	0x1f83d9ab,
+	0x5be0cd19
+};
+
+uint32_t rotate_right(uint32_t i, uint8_t d) { return (i >> d) | (i << (32 - d)); }
 
 // message len is the number of bits in the message
 // returns the number of bytes for the message block
@@ -16,19 +46,70 @@ uint64_t get_message_block_bytes(uint64_t message_bits)
 void prepare(uint8_t* message, uint64_t message_bits, uint8_t* message_block, uint64_t message_block_bytes)
 {
 	// PERF: figure out if we can do mem copy
-	for (uint64_t i = 0; i < message_bits / 8; i++)
+	for (uint8_t i = 0; i < message_bits / 8; i++)
 	{
 		message_block[i] = message[i];
 	}
 
 	message_block[message_bits / 8] |= (1 << 7);
 
-	for (uint64_t i = 0; i < 8; i++)
+	for (uint8_t i = 0; i < 8; i++)
 	{
 		message_block[message_block_bytes - 1 - i] = (message_bits >> (i * 8)) & 0xff;
 	}
 }
 
 // message_schedule is 64 32 bit ints
-void hash(uint32_t* message_schedule)
-{}
+// TODO: for now we are assuming message_block is exactly 512 bits
+void hash(uint32_t *message_schedule, uint8_t* message_block, uint8_t* out)
+{
+	for (uint8_t i = 0; i < 16; i++)
+	{
+		message_schedule[i] = (message_block[i*4] << 24) | (message_block[(i*4)+1] << 16) | (message_block[(i*4)+2] << 8) | message_block[(i*4)+3];
+	}
+
+	for (uint8_t word_i = 16; word_i < 64; word_i++)
+	{
+		uint32_t sig0 = rotate_right(message_schedule[word_i - 15], 7)
+			^ rotate_right(message_schedule[word_i - 15], 18)
+			^ (message_schedule[word_i - 15] >> 3);
+		uint32_t sig1 = rotate_right(message_schedule[word_i - 2], 17)
+			^ rotate_right(message_schedule[word_i - 2], 19)
+			^ (message_schedule[word_i - 2] >> 10);
+		message_schedule[word_i] = message_schedule[word_i - 16] + sig0 + message_schedule[word_i - 7] + sig1;
+	}
+
+	uint32_t w[8];
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		w[i] = H[i];
+	}
+
+	for (uint8_t i = 0; i < 64; i++)
+	{
+		uint32_t sig0 = rotate_right(w[0], 2) ^ rotate_right(w[0], 13) ^ rotate_right(w[0], 22);
+		uint32_t sig1 = rotate_right(w[4], 6) ^ rotate_right(w[4], 11) ^ rotate_right(w[4], 25);
+		uint32_t choice = (w[4] & w[5]) ^ ((~ w[4]) & w[6]);
+		uint32_t maj = (w[0] & w[1]) ^ (w[0] & w[2]) ^ (w[1] & w[2]);
+		uint32_t temp1 = w[7] + sig1 + choice + K[i] + message_schedule[i];
+		uint32_t temp2 = sig0 + maj;
+
+		w[7] = w[6];
+		w[6] = w[5];
+		w[5] = w[4];
+		w[4] = w[3] + temp1;
+		w[3] = w[2];
+		w[2] = w[1];
+		w[1] = w[0];
+		w[0] = temp1 + temp2;
+	}
+
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		uint32_t h = H[i] + w[i];
+		for (uint8_t j = 0; j < 4; j++)
+		{
+			out[(i*4) + 3 - j] = (h >> (j * 8)) & 0xff;
+		}
+	}
+}
--- test.c
+++ test.c
@@ -3,24 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void print_bytes(uint8_t* bytes, uint64_t len)
 {
-	uint8_t message[] = "hello, my name is macintosh!, and yeah, idk man, its hard to say";
-	uint64_t message_bits = (sizeof(message) - 1) * 8;
 
-	uint64_t message_block_bytes = get_message_block_bytes(message_bits);
-	uint8_t* message_block = malloc(message_block_bytes);
-
-	prepare(message, message_bits, message_block, message_block_bytes);
-
-	printf("message bits: %d\n\n", message_bits);
-	printf("message block bytes: %d\n\n", message_block_bytes);
-	printf("message block:\n");
-	for (int i = 0; i < message_block_bytes / 4; i++)
+	for (int i = 0; i < len / 4; i++)
 	{
 		for (int j = i * 4; j < (i * 4) + 4; j++)
 		{
-			char val = message_block[j];
+			uint8_t val = bytes[j];
 			
 			for (int i = 7; 0 <= i; i--)
 			{
@@ -32,3 +22,42 @@ int main()
 	}
 }
 
+void print_words(uint32_t* words, uint64_t len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		uint32_t word = words[i];
+		for (int i = 31; 0 <= i; i--)
+		{
+			printf("%u", (word >> i) & 1);
+		}
+		printf(": %d\n", i);
+	}
+}
+
+int main()
+{
+	uint8_t message[] = "hello, my name is macintosh!,";
+	uint64_t message_bits = (sizeof(message) - 1) * 8;
+
+	uint64_t message_block_bytes = get_message_block_bytes(message_bits);
+	uint8_t* message_block = malloc(message_block_bytes);
+
+	prepare(message, message_bits, message_block, message_block_bytes);
+
+	printf("message bits: %llu\n\n", message_bits);
+	printf("message block bytes: %llu\n\n", message_block_bytes);
+	printf("message block:\n");
+	print_bytes(message_block, message_block_bytes);
+	printf("\n\n");
+
+	uint32_t message_schedule[64] = {0};
+	uint8_t out[32] = {0};
+	hash(message_schedule, message_block, out);
+	printf("message schedule:\n");
+	print_words(message_schedule, 64);
+	printf("\n\n");
+
+	printf("hash:\n");
+	print_bytes(out, 32);
+}
